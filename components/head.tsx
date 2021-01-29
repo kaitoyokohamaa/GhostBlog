bafe67c03d15c23ebcd9ca5595d85a30f0aa9a79
yoko <s1A121700885@toyo.jp>
2021-01-29 10:34:31 +0900

    feat:SEO対策

--- components/head.tsx
+++ components/head.tsx
@@ -21,11 +21,15 @@ const HeadConfigurations = ({
       <html lang={lang} />
       <Head>
         <title>{title}</title>
+
         <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
+        <meta
+          name="Description"
+          property="og:description"
+          content={description}
+        />
         <meta name="keywords" content={keyword} />
         <meta property="og:type" content="blog" />
-
         <meta property="og:image" content={image} />
         <meta property="og:site_name" content={title} />
         <meta name="twitter:card" content="summary" />
@@ -34,7 +38,6 @@ const HeadConfigurations = ({
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={image} />
-
         <link rel="shortcut icon" href={image} />
         <link rel="apple-touch-icon" href={image} />
       </Head>
